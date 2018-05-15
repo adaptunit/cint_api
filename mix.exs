c8e30aeed700e7354cd885c6146a1f30f0749d4f
amigo <adaptunit@gmail.com>
2018-05-15 16:50:04 +0300

    elixir ~> 1.4

--- mix.exs
+++ mix.exs
@@ -5,7 +5,7 @@ defmodule CintApi.Mixfile do
     [
       app: :cint_api,
       version: "0.1.0",
-      elixir: "~> 1.5",
+      elixir: "~> 1.4",
       start_permanent: Mix.env == :prod,
       deps: deps()
     ]
