109d41a297f9263ccc1eac221c721b842fa18333
amigo <adaptunit@gmail.com>
2018-06-20 18:49:39 +0300

    +struct description

--- lib/structure/candidate_respondent.ex
+++ lib/structure/candidate_respondent.ex
@@ -1,3 +1,21 @@
 defmodule CandidateRespondent do
-    defstruct [:respondent_params, :quota_ids, :allow_routing, :min_cpi, :min_cr, :min_ir, :auto_accept_invitation]
+    defstruct [:respondent_params, :quota_ids, :allow_routing, :min_cpi, :min_cr, :min_ir, :max_loi, :auto_accept_invitation]
+
+    @optional_fields ~w(respondent_params quota_ids allow_routing min_cpi min_cr min_ir max_loi auto_accept_invitation)
+
+    schema "candidate_respondent" do
+        field :respondent_params, :string
+        field :quota_ids, :array
+        field :allow_routing, :boolean
+        field :min_cpi, :float
+        field :min_cr, :integer
+        field :min_ir, :integer
+        field :max_loi, :integer
+        field :auto_accept_invitation, :boolean
+    end
+
+    def changeset(struct, params \\ %{}) do
+        struct
+        |> cast(params, @optional_fields)
+    end
 end
